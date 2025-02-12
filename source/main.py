f0d17d4cecf18315305ec5a5fc4733a78fb9ae77
masaya <m.shimotani.sys24@morijyobi.ac.jp>
2025-02-12 15:04:53 +0900

    売上のリセット close #8

--- source/main.py
+++ source/main.py
@@ -5,19 +5,19 @@ def payment_process():
     total = 0
     if buy[0] != 0:
         payment_message += f'特製ラーメン {buy[0]}個'
-        total += 1000 * buy[0]
+        total += price[0] * buy[0]
     
     if buy[1] != 0:
         payment_message += f'醤油ラーメン {buy[1]}個'
-        total += 780 * buy[1]
+        total += price[1] * buy[1]
         
     if buy[2] != 0:
         payment_message += f'塩ラーメン {buy[2]}個'
-        total += 880 * buy[2]
+        total += price[2] * buy[2]
         
     if buy[3] != 0:
         payment_message += f'ごはん {buy[3]}個'
-        total += 150 * buy[3]
+        total += price[3] * buy[3]
         
     print(f'合計金額{total}円です。')
     
@@ -35,47 +35,68 @@ def payment_process():
         # 合計金額以上だった場合お釣りを返して終了
         else:
             balance = int(balance)
-            print(f'お釣り{balance - total}円です。')
+            print(f'お釣り{balance - total}円です。\n')
             break
     
-
-print('券売機シミュレータ')
+price = [1000, 780, 880, 150]
 buy = [0, 0, 0, 0]
 
 # 最初の処理を選ぶ
 while True:
-    result = input('券売機処理 : 1\n管理画面 : 2\n終了 : 3\n')
+    while True:
+        print('券売機シミュレータ')
+        result = input('券売機処理 : 1\n管理画面 : 2\n終了 : 3\n')
     # 数字であり０より大きく４より小さい場合次の処理に
-    if result.isdigit() and int(result) > 0 and int(result) < 4:
-        result = int(result)
-        break
+        if result.isdigit() and int(result) > 0 and int(result) < 4:
+            result = int(result)
+            break
     # それ以外の場合エラー文を表示して再度受け取り処理
-    else:
-        print('指定された数字の中から入力してください。')
+        else:
+            print('指定された数字の中から入力してください。')
         
 
 # 1が選ばれた場合
-if result == 1:
-    print('1.特製ラーメン 1000円\n2.醤油ラーメン 780円\n3.塩ラーメン 880円\n4.ごはん 150円\n----\n')
-    while True:
-        tmp = input('購入する商品番号(支払いに進む場合はc) >>> ')
-        if tmp.isdigit() and int(tmp) > 0 and int(tmp) < 5:
-            tmp = int(tmp)
-            print(f'{tmp}番が選択されました。')
-            buy[tmp - 1] += 1
-            print(buy)
-        elif tmp == 'c':
-            if buy[0] == 0 and buy[1] == 0 and buy[2] == 0 and buy[3] == 0:
-                print('商品を選択してください。')
+    if result == 1:
+        print(f'1.特製ラーメン {price[0]}円\n2.醤油ラーメン {price[1]}円\n3.塩ラーメン {price[2]}円\n4.ごはん {price[3]}円\n----\n')
+        while True:
+            tmp = input('購入する商品番号(支払いに進む場合はc) >>> ')
+            if tmp.isdigit() and int(tmp) > 0 and int(tmp) < 5:
+                tmp = int(tmp)
+                print(f'{tmp}番が選択されました。')
+                buy[tmp - 1] += 1
+                print(buy)
+            elif tmp == 'c':
+                if buy[0] == 0 and buy[1] == 0 and buy[2] == 0 and buy[3] == 0:
+                    print('商品を選択してください。')
+                else:
+                    print('支払処理')
+                    payment_process()
+                    break
             else:
-                print('支払処理')
-                payment_process()
-                break
-        else:
-            print('指定された数字から入力してください。')
+                print('指定された数字から入力してください。')
+        
+    # 2が選ばれた時に管理画面
+    elif result == 2:
+        while True:
+            print(f'1.特製ラーメン {price[0]}円 {buy[0]}個販売 合計{price[0] * buy[0]}円\n2.醤油ラーメン {price[1]}円 {buy[1]}個販売 合計{price[1] * buy[1]}円\n3.塩ラーメン {price[2]}円 {buy[2]}個販売 合計{price[2] * buy[2]}円\n4.ごはん {price[3]}円 {buy[3]}個販売 合計 {price[3] * buy[3]}円\n---\n総売上金額 {price[0] * buy[0] + price[1] * buy[1] + price[2] * buy[2] + price[3] * buy[3]}円')
+            manager_result = input('---管理メニュー---\n1.売上をリセットする\n2.商品の価格を変更する\n※売上がリセットされていないと利用できません。\n3.管理画面を終了する\n---\n管理コード入力 >>> ')
+            
+            if not manager_result.isdigit():
+                print('数字を入力してください。')
+            elif int(manager_result) > 3 or int(manager_result) < 1:
+                print('1から3の間で入力してください。')
+            elif int(manager_result) == 1:
+                buy = [0, 0, 0, 0]
+                print('売上をリセットしました。')
+            elif int(manager_result) == 2:
+                pass
+            else:
+                pass
+                # break
+                
 
 # 3が選ばれた時に終了
-if result == 3:
-    print('終了が選ばれました。')
-    sys.exit()
+    else:
+        print('終了が選ばれました。')
+        sys.exit()
     
