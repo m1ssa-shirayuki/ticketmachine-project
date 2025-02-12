24f359e378c09211bfbbaa08c2f1da01c3276cbc
masaya <m.shimotani.sys24@morijyobi.ac.jp>
2025-02-12 13:18:16 +0900

    特定の文字が入力された場合に一覧と合計を表示する機能 close #15

--- source/main.py
+++ source/main.py
@@ -1,7 +1,28 @@
 import sys
 
+def payment_process():
+    payment_message = '購入一覧'
+    total = 0
+    if buy[0] != 0:
+        payment_message += f'特製ラーメン {buy[0]}個'
+        total += 1000 * buy[0]
+    
+    if buy[1] != 0:
+        payment_message += f'醤油ラーメン {buy[1]}個'
+        total += 780 * buy[1]
+        
+    if buy[2] != 0:
+        payment_message += f'塩ラーメン {buy[2]}個'
+        total += 880 * buy[2]
+        
+    if buy[3] != 0:
+        payment_message += f'ごはん {buy[3]}個'
+        total += 150 * buy[3]
+        
+    print(f'合計金額{total}円です。')
+
 print('券売機シミュレータ')
-buy = []
+buy = [0, 0, 0, 0]
 
 # 最初の処理を選ぶ
 while True:
@@ -12,7 +33,7 @@ while True:
         break
     # それ以外の場合エラー文を表示して再度受け取り処理
     else:
-        print('指定された数字の中から入力して下さい。')
+        print('指定された数字の中から入力してください。')
         
 
 # 1が選ばれた場合
@@ -21,10 +42,17 @@ if result == 1:
     while True:
         tmp = input('購入する商品番号(支払いに進む場合はc) >>> ')
         if tmp.isdigit() and int(tmp) > 0 and int(tmp) < 5:
-            print('商品選択時の処理')
+            tmp = int(tmp)
+            print(f'{tmp}番が選択されました。')
+            buy[tmp - 1] += 1
+            print(buy)
         elif tmp == 'c':
-            print('支払処理')
-            break
+            if buy[0] == 0 and buy[1] == 0 and buy[2] == 0 and buy[3] == 0:
+                print('商品を選択してください。')
+            else:
+                print('支払処理')
+                payment_process()
+                break
         else:
             print('指定された数字から入力してください。')
 
@@ -32,3 +60,4 @@ if result == 1:
 if result == 3:
     print('終了が選ばれました。')
     sys.exit()
+    
